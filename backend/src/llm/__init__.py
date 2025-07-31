fa1cd926eff59ab5bc7916494ba9ad22d572885e
Jessica Holding <54175117+SLjholding@users.noreply.github.com>
2025-07-31 14:16:12 +0100

    OSAI-18 Create LLM files for LM Studio to be used by different agents (#5)
    * OSAI-18 Create lmstudio files with schemas for different agents

--- backend/src/llm/__init__.py
+++ backend/src/llm/__init__.py
@@ -3,5 +3,19 @@ from .factory import get_llm
 from .mistral import Mistral
 from .count_calls import count_calls
 from .openai import OpenAI
+from .lmstudio_answer import LmStudioAnswer
+from .lmstudio_intent import LmStudioIntent
+from .lmstudio_suggestions import LmStudioSuggestions
+from .lmstudio_web import LmStudioWeb
 
-__all__ = ["count_calls", "get_llm", "LLM", "LLMFile", "Mistral", "OpenAI"]
+__all__ = [
+    "count_calls",
+    "get_llm",
+    "LLM",
+    "LLMFile",
+    "Mistral",
+    "OpenAI",
+    "LmStudioAnswer",
+    "LmStudioIntent",
+    "LmStudioSuggestions",
+    "LmStudioWeb"]
