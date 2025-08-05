a4c722251f086c02a466e0b36e6279aa5558c2f4
SCOTT\afonseca <AFonseca@ScottLogic.com>
2025-08-05 10:16:27 +0100

    OSAI-41 set up ragas

---
+++ ragas_eval.py
@@ -0,0 +1,33 @@
+import os
+import asyncio
+
+open_api_key = os.getenv("OPENAI_KEY")
+os.environ["OPENAI_API_KEY"] = open_api_key if open_api_key else ""
+
+from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from ragas import SingleTurnSample
+from ragas.metrics import AspectCritic
+
+
+evaluator_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4o"))
+evaluator_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings())
+
+est_data = {
+    "user_input": "What is the percentage of reduction of the baseline emissions for Scope 1 and Scope 2?",
+    "response": "Scott Logic has set a target of 100% to achieve a reduction in their baseline emissions for Scope 1 and Scope 2 by the year 2026",
+}
+
+metric = AspectCritic(name="summary_accuracy",llm=evaluator_llm, definition="Verify if the summary is accurate.")
+test_data = SingleTurnSample(**est_data)
+
+
+
+async def evaluate_summary():
+    result = await metric.single_turn_ascore(test_data)
+    print(f"Result: {result}")
+
+# Run the async function
+asyncio.run(evaluate_summary())
