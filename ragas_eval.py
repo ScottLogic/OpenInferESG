c8f38c39089e02e2de7f2dca95aaaad4e2428c06
SCOTT\afonseca <AFonseca@ScottLogic.com>
2025-08-06 08:14:30 +0100

    Adressing changes on text and adding comments

--- ragas_eval.py
+++ ragas_eval.py
@@ -2,11 +2,12 @@ import os
 import asyncio
 from dotenv import load_dotenv
 
+#Load OpenAI API Key
 load_dotenv()
 open_api_key = os.getenv("OPENAI_KEY") 
-
 os.environ["OPENAI_API_KEY"] = open_api_key if open_api_key else ""
 
+#Set up LLM and Embeddings
 from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
 from langchain_openai import ChatOpenAI
@@ -14,20 +15,23 @@ from langchain_openai import OpenAIEmbeddings
 from ragas import SingleTurnSample
 from ragas.metrics import AspectCritic
 
-
+# Initialize LLM and Embeddings for Evaluation
 evaluator_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4o"))
 evaluator_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings())
 
-est_data = {
+# Create a Sample Input
+test_data = {
     "user_input": "What is the percentage of reduction of the baseline emissions for Scope 1 and Scope 2?",
-    "response": "Scott Logic has set a target of 10% to achieve a reduction in their baseline emissions for Scope 1 and Scope 2 by the year 2026",
+    "response": "Scott Logic has set a target of to achieve a reduction in their baseline emissions for Scope 1 and Scope 2 by the year 2026",
 }
-
+# Define a Custom Metric
 metric = AspectCritic(name="summary_accuracy",llm=evaluator_llm, definition="Verify if the summary is accurate.")
-test_data = SingleTurnSample(**est_data)
+
+# Create a Single Evaluation Sample
+test_data = SingleTurnSample(**test_data)
 
 
-
+# Define a Custom Metric
 async def evaluate_summary():
     result = await metric.single_turn_ascore(test_data)
     print(f"Result: {result}")
