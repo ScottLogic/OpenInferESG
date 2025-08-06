746397c3e778e13190abd1918b871e64eae3e89d
Jessica Holding <54175117+SLjholding@users.noreply.github.com>
2025-08-06 16:52:40 +0100

    OSAI-32 ignore order of list in tests

--- backend/tests/agents/chat_agents_test.py
+++ backend/tests/agents/chat_agents_test.py
@@ -14,43 +14,43 @@ config.materiality_agent_model = "materiality_agent_model"
 
 def test_get_chat_agents_given_config_returns_list():
     config.allowed_chat_agents = ["WebAgent", "FileAgent"]
-    agents = get_chat_agents()
-    assert len(agents) == 2
-    assert any(agent.name == "WebAgent" for agent in agents)
-    assert any(agent.name == "FileAgent" for agent in agents)
+    agent_names = [a.name for a in get_chat_agents()]
+    assert len(agent_names) == 2
+    assert "WebAgent" in agent_names
+    assert "FileAgent" in agent_names
 
 def test_get_chat_agents_returns_default_list_if_empty_env_var():
     config.allowed_chat_agents = []
-    agents = get_chat_agents()
-    assert len(agents) == 4
-    assert agents[0].name == "DatastoreAgent"
-    assert agents[1].name == "WebAgent"
-    assert agents[2].name == "MaterialityAgent"
-    assert agents[3].name == "FileAgent"
+    agent_names = [a.name for a in get_chat_agents()]
+    assert len(agent_names) == 4
+    assert "DatastoreAgent" in agent_names
+    assert "WebAgent" in agent_names
+    assert "MaterialityAgent" in agent_names
+    assert "FileAgent" in agent_names
 
 def test_get_chat_agents_returns_default_list_if_no_env_var():
     config.allowed_chat_agents = None
-    agents = get_chat_agents()
-    assert len(agents) == 4
-    assert agents[0].name == "DatastoreAgent"
-    assert agents[1].name == "WebAgent"
-    assert agents[2].name == "MaterialityAgent"
-    assert agents[3].name == "FileAgent"
+    agent_names = [a.name for a in get_chat_agents()]
+    assert len(agent_names) == 4
+    assert "DatastoreAgent" in agent_names
+    assert "WebAgent" in agent_names
+    assert "MaterialityAgent" in agent_names
+    assert "FileAgent" in agent_names
 
 def test_get_chat_agents_skips_unknown_agents():
     config.allowed_chat_agents = ["WebAgent", "UnknownAgent"]
     with patch('logging.Logger.warning') as mocked_logger:
-        agents = get_chat_agents()
-        assert len(agents) == 1
-        assert agents[0].name == "WebAgent"
+        agent_names = [a.name for a in get_chat_agents()]
+        assert len(agent_names) == 1
+        assert "WebAgent" in agent_names
         mocked_logger.assert_called_once_with("Skipped invalid chat agents: unknownagent")
 
 def test_get_chat_agents_removes_duplicates():
     config.allowed_chat_agents = ["WebAgent", "WebAgent", "FileAgent"]
-    agents = get_chat_agents()
-    assert len(agents) == 2
-    assert any(agent.name == "WebAgent" for agent in agents)
-    assert any(agent.name == "FileAgent" for agent in agents)
+    agent_names = [a.name for a in get_chat_agents()]
+    assert len(agent_names) == 2
+    assert "WebAgent" in agent_names
+    assert "FileAgent" in agent_names
 
 def test_get_chat_agents_errors_when_all_agents_skipped():
     config.allowed_chat_agents = ["UnknownAgent"]
