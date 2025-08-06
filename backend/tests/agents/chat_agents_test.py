0e23273e8b9bb91ea15b8da04b79132a5a8d53de
Jessica Holding <54175117+SLjholding@users.noreply.github.com>
2025-08-06 11:44:49 +0100

    OSAI-32 Unit tests for get_chat_agents

--- backend/tests/agents/chat_agents_test.py
+++ backend/tests/agents/chat_agents_test.py
@@ -1,14 +1,49 @@
-import os
 import pytest
-from pytest import raises
-from unittest import mock
+from unittest.mock import patch
 
-from src.agents import get_chat_agents
+from src.agents import get_chat_agents, config
 
-def test_get_chat_agents_returns_list(monkeypatch):
-    # mocker.patch("src.utils.config.allowed_chat_agents", [])
-    # with mock.patch.dict(os.environ, {"ALLOWED_CHAT_AGENTS": "WebAgent"}):
-    # mock environment here
-
+def test_get_chat_agents_given_config_returns_list():
+    config.allowed_chat_agents = ["WebAgent", "FileAgent"]
     agents = get_chat_agents()
-    assert len(agents) == 1
+    assert len(agents) == 2
+    assert agents[0].name == "WebAgent"
+    assert agents[1].name == "FileAgent"
+
+def test_get_chat_agents_returns_default_list_if_empty_env_var():
+    config.allowed_chat_agents = []
+    agents = get_chat_agents()
+    assert len(agents) == 4
+    assert agents[0].name == "DatastoreAgent"
+    assert agents[1].name == "WebAgent"
+    assert agents[2].name == "MaterialityAgent"
+    assert agents[3].name == "FileAgent"
+
+def test_get_chat_agents_returns_default_list_if_no_env_var():
+    config.allowed_chat_agents = None
+    agents = get_chat_agents()
+    assert len(agents) == 4
+    assert agents[0].name == "DatastoreAgent"
+    assert agents[1].name == "WebAgent"
+    assert agents[2].name == "MaterialityAgent"
+    assert agents[3].name == "FileAgent"
+
+def test_get_chat_agents_skips_unknown_agents():
+    config.allowed_chat_agents = ["WebAgent", "UnknownAgent", "OtherAgent"]
+    with patch('logging.Logger.warning') as mocked_logger:
+        agents = get_chat_agents()
+        assert len(agents) == 1
+        assert agents[0].name == "WebAgent"
+        mocked_logger.assert_called_once_with("Skipped invalid chat agents: UnknownAgent, OtherAgent")
+
+def test_get_chat_agents_removes_duplicates():
+    config.allowed_chat_agents = ["WebAgent", "WebAgent", "FileAgent"]
+    agents = get_chat_agents()
+    assert len(agents) == 2
+    assert agents[0].name == "WebAgent"
+    assert agents[1].name == "FileAgent"
+
+def test_get_chat_agents_errors_when_all_agents_skipped():
+    config.allowed_chat_agents = ["UnknownAgent"]
+    with pytest.raises(Exception, match="No valid chat agents found in ALLOWED_CHAT_AGENTS configuration."):
+        get_chat_agents()
