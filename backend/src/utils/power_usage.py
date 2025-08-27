ca8fa1eb9776b7c3e3ab2ccadb1468e35f3b043f
Jessica Holding <54175117+SLjholding@users.noreply.github.com>
2025-08-27 16:03:33 +0100

    OSAI-51 Lint fixes

--- backend/src/utils/power_usage.py
+++ backend/src/utils/power_usage.py
@@ -32,7 +32,9 @@ def calculate_power_usage(duration_seconds: float, model: str) -> float | None:
     global model_power_dict
     if model_power_dict is None:
         openai_power_stats = json.load(open(JSON_FILE_PATH))
-        model_power_dict = dict(map(lambda key: (key, calculate_model_power(key, openai_power_stats)), openai_power_stats.keys()))
+        model_power_dict = dict(
+            map(lambda key: (key, calculate_model_power(key, openai_power_stats)), openai_power_stats.keys())
+        )
 
     model_power = model_power_dict.get(model)
     if model_power is None:
